2f1782e9e8eb9ea6368532d58f36eba55c070fe4
shivarao18 <shiva.thummanapalli@gmail.com>
2025-05-12 16:01:11 -0400

    Instructions done

--- anchor/voting/programs/voting/src/lib.rs
+++ anchor/voting/programs/voting/src/lib.rs
@@ -1,23 +1,49 @@
 use anchor_lang::prelude::*;
 
-declare_id!("HeGwj8AH7xAa4tatAK9YDL7cZYdjTynnrfAuvG6oUfm3");
+declare_id!("5s3PtT8kLYCv1WEp6dSh3T7EuF35Z6jSu5Cvx4hWG79H");
 
 #[program]
 pub mod voting {
     use super::*;
 
     pub fn initialize_poll(ctx: Context<InitializePoll>, 
-        _poll_id: u64, 
-        start_time: u64, 
-        end_time: u64,
-        name: String,
-        description: String) -> Result<()> {
+                            _poll_id: u64, 
+                            start_time: u64, 
+                            end_time: u64,
+                            name: String,
+                            description: String) -> Result<()> {
         ctx.accounts.poll_account.poll_name = name;
         ctx.accounts.poll_account.poll_description = description;
         ctx.accounts.poll_account.poll_voting_start = start_time;
         ctx.accounts.poll_account.poll_voting_end = end_time;
         Ok(())
     }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, 
+                                _poll_id: u64, 
+                                candidate: String) -> Result<()> {
+        ctx.accounts.candidate_account.candidate_name = candidate;
+        ctx.accounts.poll_account.poll_option_index += 1;
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>, _poll_id: u64, _candidate: String) -> Result<()> {
+        let candidate_account = &mut ctx.accounts.candidate_account;
+        let current_time = Clock::get()?.unix_timestamp;
+
+        if current_time > (ctx.accounts.poll_account.poll_voting_end as i64) {
+            return Err(ErrorCode::VotingEnded.into());
+        }
+
+        if current_time <= (ctx.accounts.poll_account.poll_voting_start as i64) {
+            return Err(ErrorCode::VotingNotStarted.into());
+        }
+
+        candidate_account.candidate_votes += 1;
+
+        Ok(())
+    }
+    
 }
 
 #[derive(Accounts)]
@@ -38,6 +64,54 @@ pub struct InitializePoll<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + CandidateAccount::INIT_SPACE,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate.as_ref()],
+        bump
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        mut,
+        seeds = [poll_id.to_le_bytes().as_ref(), candidate.as_ref()],
+        bump)]
+    pub candidate_account: Account<'info, CandidateAccount>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateAccount {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct PollAccount{
@@ -50,3 +124,10 @@ pub struct PollAccount{
     pub poll_option_index: u64,
 }
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Voting has not started yet")]
+    VotingNotStarted,
+    #[msg("Voting has ended")]
+    VotingEnded,
+}
--- anchor/voting/tests/voting.js
+++ anchor/voting/tests/voting.js
@@ -1,13 +1,87 @@
-const anchor = require("@coral-xyz/anchor");
+// will try to write pusedo code since there is a lot goin on.
 
-describe("voting", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+//first intuition , i need ot set up a enviroment where my program can be deployed , BellPlus;loy my program there 
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Voting;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+/* 
+importing anchor makes sense
+import * as anchor frrom "@coral-xyz/anchor"
+import {BN, Program } from "@coral-xyz/anchor" , BN makes sense, but cna be directly used form anchor aswell , lets check if we will need progrma somewhere
+
+Testign libraries import : 
+import { startAnchor } from "solana-bankrun"; (this one creates the environment, with oput program in it)
+import { BankrunProvider } from "anchor-bankrun"; (we can use this wrapoer to interacct with out environment)
+
+import { PublicKey } from '@solana/web3.js'; (maybe we will need this in development as well, let see where will use thsi )
+
+
+
+
+//// Now , things we need from our code base , that is the program we need to actually tst, we also need that.
+
+we need idl/ ABI generated during anchor build , similar to hardhat
+const IDL = require("../target/idl/voting.json");
+
+we need the program itseld to be deployed
+import { Voting } from '../target/types/voting';
+
+I think in testing instead of getting the key after deploying, it will jsut get deployed at whereevr it is mentioend in out program  (doubt)
+
+const PUPPET_PROGRAM_ID = new PublicKey("5s3PtT8kLYCv1WEp6dSh3T7EuF35Z6jSu5Cvx4hWG79H");
+
+/// Actual test 
+
+first we need to deploy our contract at the beginning before performing tests:
+
+
+
+
+
+
+*/
+
+
+
+
+
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
+import { PublicKey } from '@solana/web3.js';
+import * as anchor from '@coral-xyz/anchor';
+import { BN, Program } from "@coral-xyz/anchor";
+
+
+const IDL = require("../target/idl/voting.json");
+import { Voting } from '../target/types/voting';
+
+const PUPPET_PROGRAM_ID = new PublicKey("5s3PtT8kLYCv1WEp6dSh3T7EuF35Z6jSu5Cvx4hWG79H");
+
+describe('Create a system account', () => {
+
+  test("bankrun", async () => {
+    const context = await startAnchor("", [{name: "voting", programId: PUPPET_PROGRAM_ID}], []);
+    const provider = new BankrunProvider(context);
+
+    const puppetProgram = new Program<Voting>(
+      IDL,
+      provider,
+    );
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [Buffer.from("poll"), new BN(1).toArrayLike(Buffer, "le", 8)],
+      puppetProgram.programId
+    );
+
+    await puppetProgram.methods.initializePoll(
+      new anchor.BN(1),
+        new anchor.BN(0),
+        new anchor.BN(1759508293),
+        "test-poll",
+        "description",
+    ).rpc();
+
+    const pollAccount = await puppetProgram.account.pollAccount.fetch(pollAddress);
+    console.log(pollAccount);
+
   });
+
 });
